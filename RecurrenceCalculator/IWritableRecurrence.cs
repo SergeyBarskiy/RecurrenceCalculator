e60ec00cba727d360c85bd0e23c9da420c6bc410
Sergey Barskiy <sergey@barskiy.com>
2022-12-02 11:20:38 -0500

    Add .net 7 and implement fluent builder

--- RecurrenceCalculator/IWritableRecurrence.cs
+++ RecurrenceCalculator/IWritableRecurrence.cs
@@ -3,101 +3,101 @@
 namespace RecurrenceCalculator
 {
     /// <summary>
-    /// Interface IRecurrence
-    /// Recurrence setup information
+    /// Interface IWritableRecurrence
+    /// Recurrence setup information, but with writable properties
     /// </summary>
-    public interface IRecurrence
+    public interface IWritableRecurrence : IRecurrence
     {
         /// <summary>
         /// Gets the day of month.
         /// </summary>
         /// <value>The day of month.</value>
-        int DayOfMonth { get; }
+        new int DayOfMonth { get; set; }
 
         /// <summary>
         /// Gets a value indicating whether this <see cref="IRecurrence"/> event can occur on Sunday.
         /// </summary>
         /// <value><c>true</c> if event can occur on Sunday; otherwise, <c>false</c>.</value>
-        bool Sunday { get; }
+        new bool Sunday { get; set; }
 
         /// <summary>
         /// Gets a value indicating whether this <see cref="IRecurrence"/> event can occur on Monday.
         /// </summary>
         /// <value><c>true</c>  if event can occur on Monday; otherwise, <c>false</c>.</value>
-        bool Monday { get; }
+        new bool Monday { get; set; }
 
         /// <summary>
         /// Gets a value indicating whether this <see cref="IRecurrence"/> event can occur on Tuesday.
         /// </summary>
         /// <value><c>true</c>  if event can occur on Tuesday; otherwise, <c>false</c>.</value>
-        bool Tuesday { get; }
+        new bool Tuesday { get; set; }
 
         /// <summary>
         /// Gets a value indicating whether this <see cref="IRecurrence"/> event can occur on Wednesday.
         /// </summary>
         /// <value><c>true</c>  if event can occur on Wednesday; otherwise, <c>false</c>.</value>
-        bool Wednesday { get; }
+        new bool Wednesday { get; set; }
 
         /// <summary>
         /// Gets a value indicating whether this <see cref="IRecurrence"/> event can occur on Thursday.
         /// </summary>
         /// <value><c>true</c>  if event can occur on Thursday; otherwise, <c>false</c>.</value>
-        bool Thursday { get; }
+        new bool Thursday { get; set; }
 
         /// <summary>
         /// Gets a value indicating whether this <see cref="IRecurrence"/> event can occur on Friday.
         /// </summary>
         /// <value><c>true</c>  if event can occur on Friday; otherwise, <c>false</c>.</value>
-        bool Friday { get; }
+        new bool Friday { get; set; }
 
         /// <summary>
         /// Gets a value indicating whether this <see cref="IRecurrence"/> event can occur on Saturday.
         /// </summary>
         /// <value><c>true</c>  if event can occur on Saturday; otherwise, <c>false</c>.</value>
-        bool Saturday { get; }
+        new bool Saturday { get; set; }
 
         /// <summary>
         /// Gets the instance of the occurrence, such as 1st or 2nd.  Set to 5 for the last instance
         /// </summary>
         /// <value>The of the occurrence, such as 1st or 2nd.  Set to 5 for the last instance.</value>
-        int Instance { get; }
+        new int Instance { get; set; }
         /// <summary>
         /// Gets the interval, such as every 2 days or every 3 years.
         /// </summary>
         /// <value>The interval, such as every 2 days or every 3 years.</value>
-        int Interval { get; }
+        new int Interval { get; set; }
 
         /// <summary>
         /// Gets the month of year for yearly occurrences.
         /// </summary>
         /// <value>The month of year.</value>
-        int MonthOfYear { get; }
+        new int MonthOfYear { get; set; }
 
         /// <summary>
         /// Gets the total number of occurrences to generate
         /// </summary>
         /// <value>The total number of occurrences to generate.</value>
-        int Occurrences { get; }
+        new int Occurrences { get; set; }
 
         /// <summary>
         /// Gets the start date, including time which is appointment type.
         /// </summary>
         /// <value>The start date, including time which is appointment type.</value>
-        DateTime StartDate { get; }
+        new DateTime StartDate { get; set; }
 
         /// <summary>
         /// Gets the type of the recurrence.
         /// </summary>
         /// <value>The type of the recurrence.</value>
-        RecurrenceType RecurrenceType { get; }
+        new RecurrenceType RecurrenceType { get; set; }
 
         /// <summary>
         /// Gets the end date.  Can be the last date of the event.  
-        /// Alternative to number of occurrences.
+        /// AAlternative to number of occurrences.
         /// </summary>
         /// <value>The end date.  Can be the last date of the event.  
         /// AAlternative to number of occurrences..</value>
-        DateTime? EndDate { get; }
+        new DateTime? EndDate { get; set; }
 
     }
 }
